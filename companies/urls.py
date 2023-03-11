a5883e35ee14bd56348c81b25f10374ecc1019e3
DiegoSampedro <diegosampedrovina@gmail.com>
2023-03-11 21:40:08 +0000

    added api endpoint to get a company by id

--- companies/urls.py
+++ companies/urls.py
@@ -8,5 +8,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.CompanyList.as_view(), name='list_companies'),
     url(r'^stats/$', views.company_stats_api_view, name='company_stats_api_view'),
+    path('<id>/', views.CompanyRetrieve.as_view(), name='get_company'),
     url(r'^stats/view/$', views.company_stats_view, name='company_stats_view')
 ]
