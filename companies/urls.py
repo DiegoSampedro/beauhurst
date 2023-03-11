65fef7fda6e322651b61c3571abaa8ec58035086
DiegoSampedro <diegosampedrovina@gmail.com>
2023-03-11 21:31:30 +0000

    added api endpoint to list companies

--- companies/urls.py
+++ companies/urls.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.CompanyList.as_view(), name='list_companies'),
     url(r'^stats/$', views.company_stats_api_view, name='company_stats_api_view'),
-    url(r'^stats/view/$', views.company_stats_view, name='company_stats_view'),
+    url(r'^stats/view/$', views.company_stats_view, name='company_stats_view')
 ]
