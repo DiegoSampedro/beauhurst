e460bb3559a2492f364044a755fce45ff55badf2
DiegoSampedro <diegosampedrovina@gmail.com>
2023-03-12 18:15:20 +0000

    used jquery to populate the tables with the company stats data

--- companies/static/companies/js/main.js
+++ companies/static/companies/js/main.js
@@ -1,6 +1,27 @@
 $(document).ready(function() {
     $.get("/companies/stats/", function(data) {
-        $("#most-recently-founded").text(data.most_recently_founded[0].name);
+        const $recentlyFoundedContainer = $('#most-recently-founded');
+        for (const company of data.most_recently_founded) {
+            const $el = '<tr><td>' + company.name + '</td></tr>';
+            $recentlyFoundedContainer.append($el);
+        }
+
         $("#average-employee-count").text(data.average_employee_count);
+
+        const $foundedPerQuarterContainer = $('#founded-per-quarter');
+        for (const entity of data.companies_founded_per_quarter) {
+            const $el = '<tr><td>' + entity.year + '</td><td>' + entity.quarter + '</td><td>' + entity.count + '</td></tr>';
+            $foundedPerQuarterContainer.append($el);
+        }
+
+        $("#user-with-most-companies").text(data.user_created_most_companies);
+
+        $("#user-with-most-employees").text(data.user_created_most_employees);
+
+        const $avgDealBycountryContainer = $('#avg-deal-by-country');
+        for (const entity of data.average_deal_amount_raised_by_country) {
+            const $el = '<tr><td>' + entity.country + '</td><td>' + entity.avg_amount_raised + '</td></tr>';
+            $avgDealBycountryContainer.append($el);
+        }
     });
 });
